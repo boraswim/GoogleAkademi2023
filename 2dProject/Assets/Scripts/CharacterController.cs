cbad18aa1bfdf6d22c5f9dbdf277032fdf26ccf1
boraswim <sevimbora@gmail.com>
2023-12-04 05:06:42 +0300

    Modules 7, 8 and 9
    MODÜL 7: Unity Temel Ekranları
    MODÜL 8: 2 Boyutlu Assetlerle Çalışma
    MODÜL 9: Karakter Animasyon

---
+++ 2dProject/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 0.0f;
+    private Rigidbody2D r2d;
+    private Animator _animator;
+
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            speed = 1.0f;
+            Debug.Log("Hiz 1.0f");
+        }
+        else
+        {
+            speed = 0.0f;
+            Debug.Log("Hiz 0.0f");
+        }
+        _animator.SetFloat("speed",speed);
+        r2d.velocity = new Vector2(speed,0f);
+    }
+    
+}
